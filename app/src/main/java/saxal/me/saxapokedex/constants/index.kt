247e031d9345193c2271df4c361810d96ec1d797
Alan Sax <alan@lelander.com>
2020-08-18 10:13:10 -0500

    add all pokemon types / resources

--- app/src/main/java/saxal/me/saxapokedex/constants/index.kt
+++ app/src/main/java/saxal/me/saxapokedex/constants/index.kt
@@ -6,6 +6,19 @@ class TypesId {
         const val WATER = "water"
         const val GRASS = "grass"
         const val ELECTRIC = "electric"
+        const val ROCK = "rock"
+        const val GROUND = "ground"
+        const val POISON = "poison"
+        const val GHOST = "ghost"
+        const val PSYCHIC = "psychic"
+        const val ICE = "ice"
+        const val STEEL = "steel"
+        const val FIGHTING = "fighting"
+        const val FLYING = "flying"
+        const val DRAGON = "dragon"
+        const val FAIRY = "fairy"
+        const val NORMAL = "normal"
+        const val BUG = "bug"
     }
 }
 
--- app/src/main/java/saxal/me/saxapokedex/ui/pokedex/PokedexListAdapter.kt
+++ app/src/main/java/saxal/me/saxapokedex/ui/pokedex/PokedexListAdapter.kt
@@ -28,7 +28,7 @@ class PokedexListAdapter(private var data: List<Pokemon>) :
     inner class ViewHolder(private val binding: ListPokedexTileBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(data: Pokemon) {
-            val primaryType = data.types[0]?.type?.name!!
+            val primaryType = data.types[0].type.name
 
             binding.title = data.name.capitalize()
             binding.id = "#${data.id}"
@@ -36,7 +36,7 @@ class PokedexListAdapter(private var data: List<Pokemon>) :
             binding.type1 = primaryType.capitalize()
 
             if (data.types.size > 1) {
-                binding.type2 = data.types[1]!!.type.name.capitalize()
+                binding.type2 = data.types[1].type.name.capitalize()
             }
 
             setupCellForType(binding, primaryType)
@@ -91,6 +91,113 @@ class PokedexListAdapter(private var data: List<Pokemon>) :
                     pokeballResourceId = R.drawable.card_pokeball_electric,
                     tagResourceId = R.drawable.card_tag_electric
                 )
+
+
+            TypesId.ROCK ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_rock,
+                    pokeballResourceId = R.drawable.card_pokeball_rock,
+                    tagResourceId = R.drawable.card_tag_rock
+                )
+
+            TypesId.GROUND ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_ground,
+                    pokeballResourceId = R.drawable.card_pokeball_ground,
+                    tagResourceId = R.drawable.card_tag_ground
+                )
+
+            TypesId.POISON ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_poison,
+                    pokeballResourceId = R.drawable.card_pokeball_poison,
+                    tagResourceId = R.drawable.card_tag_poison
+                )
+
+            TypesId.GHOST ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_ghost,
+                    pokeballResourceId = R.drawable.card_pokeball_ghost,
+                    tagResourceId = R.drawable.card_tag_ghost
+                )
+
+            TypesId.PSYCHIC ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_psychic,
+                    pokeballResourceId = R.drawable.card_pokeball_psychic,
+                    tagResourceId = R.drawable.card_tag_psychic
+                )
+
+            TypesId.ICE ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_ice,
+                    pokeballResourceId = R.drawable.card_pokeball_ice,
+                    tagResourceId = R.drawable.card_tag_ice
+                )
+
+            TypesId.STEEL ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_steel,
+                    pokeballResourceId = R.drawable.card_pokeball_steel,
+                    tagResourceId = R.drawable.card_tag_steel
+                )
+
+            TypesId.FIGHTING ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_fighting,
+                    pokeballResourceId = R.drawable.card_pokeball_fighting,
+                    tagResourceId = R.drawable.card_tag_fighting
+                )
+
+            TypesId.FLYING ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_flying,
+                    pokeballResourceId = R.drawable.card_pokeball_flying,
+                    tagResourceId = R.drawable.card_tag_flying
+                )
+
+            TypesId.DRAGON ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_dragon,
+                    pokeballResourceId = R.drawable.card_pokeball_dragon,
+                    tagResourceId = R.drawable.card_tag_dragon
+                )
+
+            TypesId.FAIRY ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_fairy,
+                    pokeballResourceId = R.drawable.card_pokeball_fairy,
+                    tagResourceId = R.drawable.card_tag_fairy
+                )
+
+            TypesId.NORMAL ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_normal,
+                    pokeballResourceId = R.drawable.card_pokeball_normal,
+                    tagResourceId = R.drawable.card_tag_normal
+                )
+            TypesId.BUG ->
+                setupUI(
+                    binding,
+                    backgroundResourceId = R.drawable.card_bg_bug,
+                    pokeballResourceId = R.drawable.card_pokeball_bug,
+                    tagResourceId = R.drawable.card_tag_bug
+                )
+
+
+
             else ->
                 setupUI(
                     binding,
